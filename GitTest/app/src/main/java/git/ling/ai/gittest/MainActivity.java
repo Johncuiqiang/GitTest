56c3e78df3dfbcdd8ce5176c82077fcd1da6241d
cuiqiang <cuiqiang>
2017-11-25 13:11:20 +0800

    Revert "添加检查权限的方法"
    This reverts commit 3e75bb4aac34ae8b8bfc68aa189bbe376bd3ec01.

--- GitTest/app/src/main/java/git/ling/ai/gittest/MainActivity.java
+++ GitTest/app/src/main/java/git/ling/ai/gittest/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
         initData();
-        checkPermission();
-
     }
 
     private void initView() {
@@ -31,9 +29,4 @@ public class MainActivity extends AppCompatActivity {
     private void initData() {
 
     }
-
-    private void checkPermission() {
-
-    }
-
 }
